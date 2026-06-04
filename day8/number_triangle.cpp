966ad22960b199a1b5016a6e961d510fbe1709c8
mauliksharmaa <mauliksharma.atr@gmail.com>
2026-06-04 17:49:40 +0530

    Add files via upload

---
+++ day8/number_triangle.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// Write a program to Print number triangle.
+
+int main() {
+    int rows;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << j+1 << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
